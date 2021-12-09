91a3c6d040fccaff41cef8ba7a66648700ab72f4
Vidya Ramagiri <vidya_r@Vidyas-MacBook-Pro.local>
2021-12-09 03:45:09 +0000

    Printing Stars Pattern

---
+++ Q-1.py
@@ -0,0 +1,8 @@
+def stars_pattern(n):
+    for i in range(0,n):
+        for j in range(0,i+1):
+            print("* ", end="")
+        print("\r")
+
+n = int(input("Enter n value: "))
+stars_pattern(n) 
