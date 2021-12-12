cfe4c1f9236cd0439fc316016c2393fb516e3d37
Vidya Ramagiri <vidya_r@Vidyas-MacBook-Pro.local>
2021-12-12 01:24:26 +0000

    GCD of two numbers

---
+++ GCD.py
@@ -0,0 +1,14 @@
+''' Given two positive integers A and B, find GCD of A and B.
+Example 1:
+
+Input: A = 3, B = 6
+Output: 3
+Explanation: GCD of 3 and 6 is 3
+'''
+import math
+class Solution:
+    def gcd(self, A, B):
+        return math.gcd(A,B)
+
+''' Driver code is at https://practice.geeksforgeeks.org/problems/gcd-of-two-numbers3459/1#
+'''
