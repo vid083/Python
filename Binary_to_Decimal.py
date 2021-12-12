9ab13e2371249acecde402fc7da163daf410bf94
Vidya Ramagiri <vidya_r@Vidyas-MacBook-Pro.local>
2021-12-12 00:12:01 +0000

    Binary to Decimal Conversion

---
+++ Binary_to_Decimal.py
@@ -0,0 +1,12 @@
+'''Given a Binary Number B, find its decimal equivalent.
+Example 1:
+
+Input: B = 10001000
+Output: 136
+
+'''
+class Solution:
+	def binary_to_decimal(self, str):
+		return int(str,2)   
+
+''' Driver code is at https://practice.geeksforgeeks.org/problems/binary-number-to-decimal-number3525/1# '''
