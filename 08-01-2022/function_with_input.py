4864fab9dd0eaa4168e01083098b81917ebb2427
Vidya Ramagiri <vidya_r@Vidyas-MacBook-Pro.local>
2022-01-09 00:32:37 +0530

    4th day of 30 days coding

---
+++ 08-01-2022/function_with_input.py
@@ -0,0 +1,4 @@
+def greet_with_name(name):
+    print(f"How r u {name}?")
+    print(f"How do you do {name}?")
+greet_with_name("vidya")
---
+++ 08-01-2022/positional_arguement.py
@@ -0,0 +1,4 @@
+def greet(name, location):
+    print(f"How r u {name}?")
+    print(f"Are you at {location}?")
+greet("angela", "england")
